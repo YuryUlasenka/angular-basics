b8925681ebf96ba7a33eb9a01207d64bd94db0a4
Yury Ulasenka <vlasenkoyura93@gmail.com>
2022-12-09 21:14:14 +0300

    workking with ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SwitchComponent } from './switch/switch.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwitchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
