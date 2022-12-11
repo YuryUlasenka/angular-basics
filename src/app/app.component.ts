f16ce218258e3fe4c2188a39c8dcf175840cb9ef
Yury Ulasenka <vlasenkoyura93@gmail.com>
2022-12-11 14:07:04 +0300

    implemented CRUD in service via httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Todo, TodosService} from "./todos.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-basics';
+export class AppComponent implements OnInit{
+  todos: Todo[] = [];
+
+  todoTitle: string = '';
+  loading: boolean = false;
+
+  constructor(private todosService: TodosService){
+  }
+
+  ngOnInit(): void {
+    this.fetchTodos()
+  }
+
+  addTodo() {
+    if(!this.todoTitle.trim()){
+      return;
+    }
+
+    this.todosService.addTodo({
+      title: this.todoTitle,
+      completed: false
+    }).subscribe(todo=>{
+        console.log(todo);
+        this.todos.push(todo);
+        this.todoTitle = '';
+      });
+  }
+
+  fetchTodos() {
+    this.loading = true;
+    this.todosService.fetchTodos()
+      .subscribe(todos=>{
+        this.todos = todos;
+        this.loading = false;
+      });
+  }
+
+  removeTodo(id: number | undefined) {
+    this.todosService.removeTodo(id)
+      .subscribe(()=>{
+        this.todos = this.todos.filter(t=> t.id !== id);
+      })
+  }
 }
