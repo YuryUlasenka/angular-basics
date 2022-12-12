6cf88c2b86ba86dba160cd3c82bfee7b8214b804
Yury Ulasenka <vlasenkoyura93@gmail.com>
2022-12-12 15:14:09 +0300

    added resolver

---
+++ src/app/post.resolver.ts
@@ -0,0 +1,22 @@
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {Post, PostsService} from "./posts.service";
+import {delay, Observable, of} from "rxjs";
+import {Injectable} from "@angular/core";
+
+@Injectable({providedIn: "root"})
+export class PostResolver implements Resolve<Post> {
+  constructor(private postsService: PostsService) {
+  }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<Post> | Promise<Post> | Post {
+
+    return of(this.postsService.getById(+route.params['id']))
+      .pipe(
+        delay(1500)
+      );
+  }
+
+}
+
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {ActivatedRoute, Params, Router} from "@angular/router";
-import {Post, PostsService} from "../posts.service";
+import {Post} from "../posts.service";
 
 @Component({
   selector: 'app-post',
@@ -11,13 +11,13 @@ export class PostComponent implements OnInit{
   post: Post | undefined;
   constructor(
     private route: ActivatedRoute,
-    private postService: PostsService,
     private router: Router){}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) =>{
-      this.post = this.postService.getById(+params['id']);
-    });
+    //this.post = this.route.snapshot.data['post'];
+    this.route.data.subscribe(data=>{
+      this.post = data['post'];
+    })
   }
 
   loadPost() {
