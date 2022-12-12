46c7554171086ff0beb7adf959fbd000b1e49e13
Yury Ulasenka <vlasenkoyura93@gmail.com>
2022-12-12 14:46:38 +0300

    learning routes

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Post, PostsService} from "../posts.service";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+  post: Post | undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService,
+    private router: Router){}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) =>{
+      this.post = this.postService.getById(+params['id']);
+    });
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44])
+  }
 }
