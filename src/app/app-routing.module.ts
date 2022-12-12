c4372adf81a46dbe8bf3bac7528a3418b4fd38e2
Yury Ulasenka <vlasenkoyura93@gmail.com>
2022-12-12 22:29:49 +0300

    split into separate modules and make lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {
+    path: 'about',
+    loadChildren: ()=> import('./about-page/about-page.module').then(x => x.AboutPageModule)}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: 'about', component: AboutPageComponent, children: [
-      {path: 'extra', component: AboutExtraPageComponent}
-    ]},
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
-  ])],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
